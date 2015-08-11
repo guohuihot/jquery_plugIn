9504bdf6ccf351ebbe82edae0f4b8e9377b2e269
guohui <guohuihot@163.com>
2015-08-11 17:40:55 +0800

    jqfixed完成

--- jqfixed/js/jqfixed.dev.js
+++ jqfixed/js/jqfixed.dev.js
@@ -2,22 +2,24 @@
 * author : ahuing
 * date   : 2015-8-7
 * name   : jqfixed v1.0
-* modify : 2015-8-10 14:43:10
+* modify : 2015-8-11 15:42:35
  */
 !function ($) {
     var Fixed = function (self, opt) {
-        this.o     = $.extend({}, Fixed.defaults, opt)
+        this.o     = $.extend(true, {}, Fixed.defaults, opt)
         this.$self = $(self)
     }
 
     Fixed.defaults = {
-        offset   : 10
-        , css    : {
+        css    : {
             top: 0
-            , position: 'static'
+            // , right : 10
+            // , display: 'none'
             , zIndex: parseInt(new Date().getTime() / 1e3)
         }
-        , bottom : 0
+        , top : 0
+        , bottom : 360
+        , close : '.btn-close'
     }
 
     Fixed.prototype = {
@@ -31,26 +33,47 @@
             , bH      = $('body').height()
             , pos     = isIE6 ? "absolute" : "fixed"
             , o       = _this.o
-            , T = this.$self.offset().top;
+            , t       = o.top || _this.$self.offset().top
+            , h       = _this.$self.outerHeight(true)
+            , _t = 0
+            , timer;
 
-            $before = $('<div></div>').insertBefore(_this.$self)
-                        .css({
-                            height: _this.$self.outerHeight(true)
-                        });
+            o.css.position = o.css.position || _this.$self.css('position');
 
-            o.css.marginTop = -_this.$self.outerHeight(true);
+            if (o.css.position != 'absolute') {
+                $('<div></div>').insertBefore(_this.$self).css({
+                    height: h
+                });
+                o.css.marginTop = -h;
+            } 
+
+            _this.$self.find(o.close).click(function () {
+                _this.$self.css('display', 'none');
+                $win.off('scroll.fixed')
+            });
+
             o.css.width = _this.$self.width();
+            if (o.css.bottom >= 0) {
+                o.css.top = wH - o.css.bottom - h
+                o.css.bottom = 'auto'
+            };
             _this.$self.css(o.css);
 
-            $win.on('scroll', function() {
-                var st = $win.scrollTop() + o.css.top;
-                st >= T && _this.$self.css({
-                    position: pos
-                    , marginTop: 0
-                }) || _this.$self.css({
-                    position: o.css.position
-                    , marginTop: o.css.marginTop
-                });
+            $win.on('scroll.fixed', function() {
+                clearTimeout(timer);
+                timer = setTimeout(function () {
+                    var st = $win.scrollTop() + (!o.top && o.css.top || 0);
+                    _this.$self
+                    .css(st >= t && {
+                        position: pos
+                        , display: 'block'
+                        , marginTop: 0
+                    } || o.css)
+                    .trigger(st > _t ? 'scrollUp' : 'scrollDown')
+                    .trigger(st > t ? 'fixed' : 'unfixed');
+
+                    _t = st;
+                }, 10)
             });
         }
     }
